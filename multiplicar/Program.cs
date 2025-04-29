b2a08e6fd60414a9d5b6792d300eae4583ea6f71
Diego Emmanuel Ramos German <diegoxemmanuel25@gmail.com>
2025-04-29 13:50:36 -0400

    h

---
+++ multiplicar/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num = 0,  num1 = 0,resultado;
+            Console.WriteLine("digital su primer numero");
+            num= Int32.Parse( Console.ReadLine());
+            Console.WriteLine("digital su segundo numero");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("digital su primer numero");
+            num = Int32.Parse(Console.ReadLine());
+            resultado = num * num1;
+            Console.WriteLine("el resultado de la multiplicacion es"+resultado);
+        }
+    }
+}
