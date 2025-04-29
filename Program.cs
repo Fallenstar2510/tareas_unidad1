fe813bc6b671ca02e32b169eeb744c9156d24684
Fallenstar2510 <Diegoxemmanuel25@gmail.com>
2025-04-29 13:42:26 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //dividir dos numeros
+
+            int num = 500;
+            int num1 = 3;
+            int resultado = num / num1;
+
+            Console.WriteLine(resultado);
+
+
+
+        }
+    }
+}
