6b28bc4149fbc9bbfe90bfb726132ca79eeb8e44
Diego Emmanuel Ramos German <diegoxemmanuel25@gmail.com>
2025-04-29 15:18:46 -0400

    h

---
+++ imprimir_impar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imprimir_impar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int numero =30; numero>0; numero--)
+            {
+
+                if (numero % 2 == 0)
+                {
+
+                    Console.WriteLine("el numero  " + numero + "  es par");
+                }
+
+            }
+
+
+        }
+    }
+}
