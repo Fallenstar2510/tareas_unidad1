918ae33a55339659ac7ec2fe2d6976d39589a99e
Diego Emmanuel Ramos German <diegoxemmanuel25@gmail.com>
2025-04-29 15:12:48 -0400

    hh

---
+++ numeroprimo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroprimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            float numero2;
+            bool primo1, primo2, primo3;
+            numero = Int32.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0 || numero % 3 == 0 || numero % 5 == 0 || numero % 7 == 0 || numero == 1)
+            {
+                if (numero != 5 && numero != 7 && numero != 2 && numero != 3 || numero ==1) 
+                { 
+                
+                Console.WriteLine("no es primo");
+                }
+                else Console.WriteLine(" es primo");
+
+            }
+            else Console.WriteLine(" es primo");
+
+
+
+        }
+    }
+}
