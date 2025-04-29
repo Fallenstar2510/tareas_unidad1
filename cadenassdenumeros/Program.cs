b89f8ba025053ddfeeeb9a2a81f4c3d306d37086
Diego Emmanuel Ramos German <diegoxemmanuel25@gmail.com>
2025-04-29 17:11:24 -0400

    hhh

---
+++ cadenassdenumeros/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadenassdenumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 0;
+            int numero2 = 1;
+            int numero3 = 0;
+            int numero4 = 0;
+            for (int numero = 1; numero2 <70; numero++)
+            {
+                
+               
+                 
+                    Console.WriteLine(numero1);
+
+                    numero2 = numero1 + numero2;
+                  numero1 = numero2 - numero1;
+                
+
+
+
+            }
+
+
+        
+    }
+    }
+}
